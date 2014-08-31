34d0527a9363c2cc0c5f298a563a1be9e96ea62d
portalzine <webmaster@portalzine.de>
2014-08-31 11:48:14 +0200

    no message

--- overview.php
+++ overview.php
@@ -83,11 +83,11 @@ if(!empty($_POST['action'])){
 <title>BroadwayAPI (PHP)</title>
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="http://bootswatch.com/flatly/bootstrap.min.css">
-<link rel="stylesheet" href="recources/css/main.css">
+<link rel="stylesheet" href="/resources/css/main.css">
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-<script src="resources/js/main.js"></script>
+<script src="/resources/js/main.js"></script>
 </head>
 <body><br>
 <div class="container">
